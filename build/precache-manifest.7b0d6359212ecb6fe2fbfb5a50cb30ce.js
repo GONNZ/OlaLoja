f9a7c7be9ad448449e5b329567a08be749294e15
gonnz <carlosluisgonzalez29@gmail.com>
2021-02-06 01:47:10 -0600

    Fix

---
+++ build/precache-manifest.7b0d6359212ecb6fe2fbfb5a50cb30ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b54af94db5d68884cacdf81e9c4eb39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "791f5a5f003eb8e36747",
+    "url": "/static/css/main.4b42fec0.chunk.css"
+  },
+  {
+    "revision": "e04e99fe7c755d7264bf",
+    "url": "/static/js/2.9d294496.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9d294496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791f5a5f003eb8e36747",
+    "url": "/static/js/main.d1be2a66.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/video-3.mp4" autoPlay loop muted />
-      <h1> STUNNING BABES <span>⚡️</span></h1>
+      <h1> STUNNING BABES <span role="img" aria-label="Tunder">⚡️</span></h1>
       <p>𝖦𝗈𝗅𝖽 𝖼𝗈𝗏𝖾𝗋𝖾𝖽 • 𝖲𝗂𝗅𝗏𝖾𝗋 𝟫𝟤𝟧 • 𝖲𝗍𝖾𝖾𝗅</p>
       <div className="hero-btns">
     
